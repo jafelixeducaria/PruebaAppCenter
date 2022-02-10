e33a3040254e78cc3c50f802413b9b8905563882
Jose Antonio Félix <jafelix@educaria.com>
2022-02-10 12:39:33 +0100

    Changes

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -22,7 +22,6 @@ npm i
 
 # run optimized production build
 npm run build -- --prod
-npx cap copy android
 
 # copy the web assets to the native projects and updates the native plugins and dependencies based in package.json
 npx cap sync
